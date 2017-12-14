c07f39f7d5502cee9e8342c5687ccf3d51376fbd
github username <github email address>
2017-12-14 16:03:05 -0500

    we did it

---
+++ challenge.js
@@ -0,0 +1,66 @@
+let number = document.getElementById('counter')
+
+let counter = 0
+
+window.addEventListener('load', function(){
+  setInterval(
+     function () {
+         number.innerText = counter
+         counter ++
+     }, 1000)
+})
+
+let likes = {}
+
+const minus = document.getElementById('-')
+const plus = document.getElementById('+')
+const like = document.getElementById('<3')
+
+minus.addEventListener('click', function(e){
+  number.innerText = counter--
+})
+
+plus.addEventListener('click', function(e){
+  number.innerText = counter++
+})
+
+like.addEventListener('click', function(){
+  if (likes[counter]){
+    likes[counter]++
+  } else {
+    likes[counter] = 1
+  }
+})
+
+const pause = document.getElementById('pause')
+
+pause.addEventListener('click', function(e){
+
+if (pause.innerText === 'pause'){
+  pause.innerText = 'resume'
+  minus.disabled = true
+  plus.disabled = true
+  like.disabled = true
+}
+else if (pause.innerText === 'resume') {
+  pause.innerText = 'pause'
+  minus.disabled = false
+  plus.disabled = false
+  like.disabled = false
+}
+
+})
+
+const listComments = document.getElementById('list')
+
+const commentBtn = document.getElementById('submit')
+
+const comment = document.getElementById('commenty')
+
+
+commentBtn.addEventListener('click', function(e){
+  e.preventDefault()
+  let newComment = document.createElement("DIV")
+  newComment.innerText = comment.value
+  listComments.append(newComment)
+})
